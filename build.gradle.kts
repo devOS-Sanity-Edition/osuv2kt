8bd51c3b8a79a03a46bc71b61b78402635ecdc84
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-09-14 01:03:32 -0700

    👷 github actions and publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    `maven-publish`
+
+    alias(libs.plugins.grgit)
 }
 
 group = "one.devos"
-version = "0.1.0"
+version = "0.1.0+rev.${grgit.head().abbreviatedId}" // im not gonna do any logic for this atm, but maybe later
 
 repositories {
     mavenCentral()
@@ -26,4 +29,40 @@ tasks.test {
 kotlin {
     explicitApi()
     jvmToolchain(21)
+}
+
+// This will attempt to publish the mod to the devOS Maven, otherwise it will build the mod locally
+// This is auto run by GitHub Actions
+task("buildOrPublish") {
+    group = "build"
+    var mavenUser = System.getenv().get("MAVEN_USER")
+    if (!mavenUser.isNullOrEmpty()) {
+        dependsOn(tasks.getByName("publish"))
+        println("prepared for publish")
+    } else {
+        dependsOn(tasks.getByName("build"))
+        println("prepared for build")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = group.toString()
+            artifactId = "osuv2kt"
+            version = version
+
+            from(components.get("java"))
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://mvn.devos.one/${System.getenv()["PUBLISH_SUFFIX"]}/")
+            credentials {
+                username = System.getenv()["MAVEN_USER"]
+                password = System.getenv()["MAVEN_PASS"]
+            }
+        }
+    }
 }
